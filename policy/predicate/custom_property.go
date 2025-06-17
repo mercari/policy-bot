ed80aa28bd5726f71d4b4407408dbc5630d5e005
Choi Chun Ming <south@mercari.com>
2025-06-17 10:28:53 +0900

    fix: license

--- policy/predicate/custom_property.go
+++ policy/predicate/custom_property.go
@@ -1,3 +1,16 @@
+// Copyright 2025 Palantir Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package predicate
 
 import (
@@ -166,6 +179,8 @@ func (pred CustomProperty) Evaluate(ctx context.Context, prctx pull.Context) (*c
 	}
 	return &predicateResult, nil
 }
+
+// Trigger returns the trigger type for the CustomProperty predicate.
 
 // Ideally we would be able to trigger on custom properties change, but it is not possible right now
 func (pred CustomProperty) Trigger() common.Trigger {
